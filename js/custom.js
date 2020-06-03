b9d27c8a74e873f5190a3a1709c79d67217ae830
anikrauth <anikrouthwebdev@gmail.com>
2020-06-04 00:17:48 +0600

    finish html markup

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    //dream slider
+    $('.dream-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false,
+
+    });
+
+
+
+
+
+
+
+
+
+});
